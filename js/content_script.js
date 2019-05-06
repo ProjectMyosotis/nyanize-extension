715db1a05a919a3b9cf4a9616147cde4442df122
hiyuki2578 <tomoka319@gmail.com>
2019-05-06 16:57:40 +0900

    jsの修正

--- js/content_script.js
+++ js/content_script.js
@@ -24,42 +24,6 @@ chrome.storage.local.get(null, function (data) {
 });
 
 function walk(node) {
-
-	var child, next;
-
-	switch (node.nodeType) {
-		case 1:
-		// Element
-		case 9:
-		// Document
-		case 11:
-			// Document fragment
-			child = node.firstChild;
-			while (child) {
-				next = child.nextSibling;
-				walk(child);
-				child = next;
-			}
-			break;
-
-		case 3:
-			// Text node
-			handleText(node);
-			break;
-	}
-}
-
-function handleText(textNode) {
-	var v = textNode.nodeValue;
-	var regex;
-
-	v = v.replace(/な/g, "にゃ");
-	v = v.replace(/ナ/g, "ニャ");
-	v = v.replace(/ﾅ/g, "ﾆｬ");
-
-	textNode.nodeValue = v;
-}
-function walk(node) {
 	var child, next;
 
 	switch (node.nodeType) {
@@ -86,6 +50,7 @@ function handleText(textNode) {
 	v = v.replace(/な/g, "にゃ");
 	v = v.replace(/ナ/g, "ニャ");
 	v = v.replace(/ﾅ/g, "ﾆｬ");
-
-	textNode.nodeValue = v;
+	if(textNode.nodeValue !== v){
+		textNode.nodeValue = v;
+	}
 }
