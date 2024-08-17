4f58607bbdfa724c96c23a3e65e1a4c2d7bfcf61
hina <hina@hinaloe.net>
2024-08-18 01:46:36 +0900

    Update bootstrap to v5

--- entrypoints/popup/main.ts
+++ entrypoints/popup/main.ts
@@ -1,68 +1,65 @@
-import './../../css/bootstrap.min.css';
-import './../../css/bootstrap-switch.min.css';
-import './../../css/main.css';
+import './style.scss';
 
 import $ from 'jquery';
 
 async function init_main() {
-  $('html').hide();
 
   let isEnyabled = 1;
   //get the current enyabled state and rule list
   const data = await browser.storage.local.get('nyanizeStatus');
-  
+
 
   console.log(data)
-  //   if (typeof data.nyanizeStatus !== "undefined") {
-  //     isEnyabled = parseInt(data.nyanizeStatus);
-  //   }
-  //   //make the switch reflect our current state
-  //   if (isEnyabled == 1) {
-  //     $('#nyanizeStatus').bootstrapSwitch('state', true);
-  //     $('#ultimatenyanizeStatus').bootstrapSwitch('state', false);
-  //   } else if (isEnyabled == 2) {
-  //     $('#nyanizeStatus').bootstrapSwitch('state', true);
-  //     $('#ultimatenyanizeStatus').bootstrapSwitch('state', true);
-  //   } else {
-  //     $('#nyanizeStatus').bootstrapSwitch('state', false);
-  //     $('#ultimatenyanizeStatus').bootstrapSwitch('state', false);
-  //   }
-  
-  // //init our switch
-  // $('#nyanizeStatus').bootstrapSwitch();
-  // $('#ultimatenyanizeStatus').bootstrapSwitch();
+  if (typeof data.nyanizeStatus !== "undefined") {
+    isEnyabled = parseInt(data.nyanizeStatus);
+  }
+  //make the switch reflect our current state
+  if (isEnyabled == 1) {
+    $('#nyanizeStatus').prop('checked', true);
+    $('#ultimatenyanizeStatus').prop('checked', false);
+  } else if (isEnyabled == 2) {
+    $('#nyanizeStatus').prop('checked', true);
+    $('#ultimatenyanizeStatus').prop('checked', true);
+  } else {
+    $('#nyanizeStatus').prop('checked', false);
+    $('#ultimatenyanizeStatus').prop('checked', false);
+  }
+
+  setupEventHandler();
 
   //show the menu
-  
+
   $('html').fadeIn('slow');
+}
+
+function setupEventHandler() {
+  //handle enyabling or disabling or the extension
+  $<HTMLInputElement>('#nyanizeStatus').on('change', function (event) {
+    if (this.checked) {
+      browser.storage.local.set({
+        "nyanizeStatus": 1
+      });
+    } else {
+      $('#ultimatenyanizeStatus').prop('checked', false);
+      browser.storage.local.set({
+        "nyanizeStatus": 0
+      });
+    }
+  });
+
+  $<HTMLInputElement>('#ultimatenyanizeStatus').on('change', function (event) {
+    if (this.checked) {
+      $('#nyanizeStatus').prop('checked', true);
+      browser.storage.local.set({
+        "nyanizeStatus": 2
+      });
+    } else {
+      browser.storage.local.set({
+        "nyanizeStatus": 1
+      });
+    }
+  });
 }
 
 //bind events to dom elements
 document.addEventListener('DOMContentLoaded', init_main);
-
-// //handle enyabling or disabling or the extension
-// $('#nyanizeStatus').on('switchChange.bootstrapSwitch', function (event, state) {
-//   if (state) {
-//     browser.storage.local.set({
-//       "nyanizeStatus": 1
-//     });
-//   } else {
-//     $('#ultimatenyanizeStatus').bootstrapSwitch('state', false);
-//     browser.storage.local.set({
-//       "nyanizeStatus": 0
-//     });
-//   }
-// });
-
-// $('#ultimatenyanizeStatus').on('switchChange.bootstrapSwitch', function (event, state) {
-//   if (state) {
-//     $('#nyanizeStatus').bootstrapSwitch('state', true);
-//     browser.storage.local.set({
-//       "nyanizeStatus": 2
-//     });
-//   } else {
-//     browser.storage.local.set({
-//       "nyanizeStatus": 1
-//     });
-//   }
-// });
