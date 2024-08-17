edbed2bc446977258d59481dd9f8669cae6d0e9f
hina <hina@hinaloe.net>
2024-08-18 02:36:47 +0900

    Enable icons

--- wxt.config.ts
+++ wxt.config.ts
@@ -1,23 +1,29 @@
-import { defineConfig } from 'wxt';
+import { defineConfig } from "wxt";
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
-    manifestVersion: 3,
-    manifest: {
-        "name": "Nyanize Extension",
-        "short_name": "Nyanize",
-        "version": "3.0.0.1",
-        "description": "nyanize all website",
-        "browser_action": {
-            "default_title": "Nyanize Options",
-            "default_icon": {
-                "19": "icon/gear19.png",
-                "38": "icon/gear38.png"
-            },
-        },
-        "permissions": [
-            "storage"
-        ]
-    }
-
+  manifestVersion: 3,
+  manifest: {
+    name: "Nyanize Extension",
+    short_name: "Nyanize",
+    version: "3.0.0.1",
+    description: "nyanize all website",
+    icons: {
+        16: "/icon/gear16.png",
+        19: "/icon/gear19.png",
+        38: "/icon/gear38.png",
+        48: "/icon/gear48.png",
+        128: "/icon/gear128.png",
+    },
+    action: {
+      default_title: "Nyanize Options",
+      default_icon: {
+        "16": "icon/gear16.png",
+        "19": "icon/gear19.png",
+        "38": "icon/gear38.png",
+        "48": "icon/gear48.png",
+      },
+    },
+    permissions: ["storage"],
+  },
 });
